164dd45ec9f64428b6773054ab81ec2fcfa9b2fa
Jame9703 <ncsb1213444@outlook.com>
2025-05-10 23:36:23 +0800

    #

--- Classes/SettingsHelper.cs
+++ Classes/SettingsHelper.cs
@@ -1,4 +1,6 @@
-﻿using Windows.Storage;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.Storage;
 
 namespace 随机抽取学号.Classes
 {
@@ -16,10 +18,17 @@ namespace 随机抽取学号.Classes
     //    public static double ContentFrameBackgroundOpacity;
     //    public static int MainPageBackground;
     //}
-    public class SettingsHelper
+    public class SettingsHelper:INotifyPropertyChanged
     {
         private static readonly ApplicationDataContainer _localSettings =
             ApplicationData.Current.LocalSettings;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            SaveSetting(propertyName);  // 属性变更时触发保存
+        }
 
         // 默认值定义区域
         private const bool DefaultDarkMode = false;
@@ -29,21 +38,28 @@ namespace 随机抽取学号.Classes
         static SettingsHelper()
         {
             // 初始化时自动填充缺失的配置项
-            EnsureSetting(nameof(IsDarkMode), DefaultDarkMode);
-            EnsureSetting(nameof(FontSize), DefaultFontSize);
-            EnsureSetting(nameof(AppLanguage), DefaultLanguage);
+            //EnsureSetting(nameof(IsDarkMode), DefaultDarkMode);
+            //EnsureSetting(nameof(FontSize), DefaultFontSize);
+            //EnsureSetting(nameof(AppLanguage), DefaultLanguage);
         }
-
+        protected void SaveSetting(string key)
+        {
+            var property = this.GetType().GetProperty(key);
+            if (property != null)
+            {
+                _localSettings.Values[key] = property.GetValue(this);
+            }
+        }
         /// <summary>
         /// 确保配置项存在，不存在时写入默认值
         /// </summary>
-        private static void EnsureSetting<T>(string key, T defaultValue)
-        {
-            if (!_localSettings.Values.ContainsKey(key))
-            {
-                _localSettings.Values[key] = defaultValue;
-            }
-        }
+        //private static void EnsureSetting<T>(string key, T defaultValue)
+        //{
+        //    if (!_localSettings.Values.ContainsKey(key))
+        //    {
+        //        _localSettings.Values[key] = defaultValue;
+        //    }
+        //}
 
         /// <summary>
         /// 泛型方法获取配置值
@@ -67,28 +83,45 @@ namespace 随机抽取学号.Classes
         /// <summary>
         /// 泛型方法设置配置值
         /// </summary>
-        private static void SetValue<T>(string key, T value)
+        //private static void SetValue<T>(string key, T value)
+        //{
+        //    _localSettings.Values[key] = value;
+        //}
+        protected T GetSetting<T>(T defaultValue, [CallerMemberName] string key = null)
         {
-            _localSettings.Values[key] = value;
+            if (_localSettings.Values.TryGetValue(key, out object value))
+            {
+                return (T)value;
+            }
+            return defaultValue;
+        }
+        private string _userName;
+        private int _fontSize;
+
+        public string UserName
+        {
+            get => GetSetting("DefaultUser");
+            set
+            {
+                if (_userName != value)
+                {
+                    _userName = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
-        // 示例配置属性
-        public static bool IsDarkMode
+        public int FontSize
         {
-            get => GetValue(nameof(IsDarkMode), DefaultDarkMode);
-            set => SetValue(nameof(IsDarkMode), value);
-        }
-
-        public static int FontSize
-        {
-            get => GetValue(nameof(FontSize), DefaultFontSize);
-            set => SetValue(nameof(FontSize), value);
-        }
-
-        public static string AppLanguage
-        {
-            get => GetValue(nameof(AppLanguage), DefaultLanguage);
-            set => SetValue(nameof(AppLanguage), value);
+            get => GetSetting(14);
+            set
+            {
+                if (_fontSize != value)
+                {
+                    _fontSize = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
         /// <summary>
@@ -98,15 +131,15 @@ namespace 随机抽取学号.Classes
         {
             switch (key)
             {
-                case nameof(IsDarkMode):
-                    SetValue(key, DefaultDarkMode);
-                    break;
-                case nameof(FontSize):
-                    SetValue(key, DefaultFontSize);
-                    break;
-                case nameof(AppLanguage):
-                    SetValue(key, DefaultLanguage);
-                    break;
+                //case nameof(IsDarkMode):
+                //    SetValue(key, DefaultDarkMode);
+                //    break;
+                //case nameof(FontSize):
+                //    SetValue(key, DefaultFontSize);
+                //    break;
+                //case nameof(AppLanguage):
+                //    SetValue(key, DefaultLanguage);
+                //    break;
             }
         }
 
@@ -115,9 +148,9 @@ namespace 随机抽取学号.Classes
         /// </summary>
         public static void ResetAll()
         {
-            IsDarkMode = DefaultDarkMode;
-            FontSize = DefaultFontSize;
-            AppLanguage = DefaultLanguage;
+            //IsDarkMode = DefaultDarkMode;
+            //FontSize = DefaultFontSize;
+            //AppLanguage = DefaultLanguage;
         }
     }
 }
